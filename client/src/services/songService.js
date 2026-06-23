8160b32f4993dccfe2b348c4e16fe8dd72546868
parthbhatt45 <parthbhatt695@gmail.com>
2026-06-24 00:22:12 +0530

    Fix songs API route

--- client/src/services/songService.js
+++ client/src/services/songService.js
@@ -1,8 +1,7 @@
 const SONG_API =
-    "https://beatbyte-v2.onrender.com";
+    "https://beatbyte-v2.onrender.com/api/songs";
 
 export const getSongs = async () => {
     const response = await fetch(SONG_API);
-
     return response.json();
 };
