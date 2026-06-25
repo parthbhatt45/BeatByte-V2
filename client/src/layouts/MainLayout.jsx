f7c2f85c60c9dccf0a137516d12791ce6931328c
parthbhatt45 <parthbhatt695@gmail.com>
2026-06-25 13:42:45 +0530

    Final changes done

--- client/src/layouts/MainLayout.jsx
+++ client/src/layouts/MainLayout.jsx
@@ -1,14 +1,24 @@
+import { useLocation } from "react-router-dom";
+
 import Sidebar from "../components/Sidebar";
 import MusicPlayer from "../components/MusicPlayer";
 
 function MainLayout({ children }) {
+    const location = useLocation();
+
+    const hidePlayer =
+        location.pathname === "/login" ||
+        location.pathname === "/register";
+
     return (
         <>
             <Sidebar />
+
             <main className="main-content">
                 {children}
             </main>
-            <MusicPlayer />
+
+            {!hidePlayer && <MusicPlayer />}
         </>
     );
 }
--- client/src/services/songService.js
+++ client/src/services/songService.js
@@ -1,7 +1,7 @@
-const SONG_API =
-    "https://beatbyte-v2.onrender.com/api/songs";
+    const SONG_API =
+        "https://beatbyte-v2.onrender.com/api/songs";
 
-export const getSongs = async () => {
-    const response = await fetch(SONG_API);
-    return response.json();
-};
+    export const getSongs = async () => {
+        const response = await fetch(SONG_API);
+        return response.json();
+    };
